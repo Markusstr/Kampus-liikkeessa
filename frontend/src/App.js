8affefde184e355e29dd6390b8cf869f8b9e2c2e
johan1954 <tomi.enberg@student.lut.fi>
2020-01-10 10:08:31 +0200

    App.js updated to use routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router,
+  Switch,
+  Route,
+  Link }
+from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <Router>
+    {/* Navbar is a specific class and a component, rendered before the router */}
+    <div>
+
+      <nav>
+        <ul>
+          <Link to="/home">
+            Home
+          </Link>
+        </ul>
+        <ul>
+          <Link to="/about">
+            About
+          </Link>
+        </ul>
+        <ul>
+          <Link to="/activity">
+            Activity
+          </Link>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route path="/home">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/activity">
+          <Activity />
+        </Route>
+      </Switch>
     </div>
+  </Router>
   );
 }
 
+// Routers rendered as functions. Replace these with function or class components when components ready 
+function Home () {
+  return (
+    <div>Homepage</div>
+  )
+}
+function About () {
+  return (
+    <div>About page</div>
+  )
+}
+function Activity () {
+  return (
+    <div>Activity page</div>
+  )
+}
 export default App;
