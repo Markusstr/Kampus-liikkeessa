420d495099c203d86a4032302ea64b5acad552ae
Markusstr <markus.strandman@gmail.com>
2020-01-13 09:40:19 +0200

    Added controllers for database

--- backend/controllers/reservationController.js
+++ backend/controllers/reservationController.js
@@ -2,7 +2,18 @@ let Reservation = require('../models/Reservation');
 
 exports.getReservations = async (req, res) => {
     try {
+        const reservation = await Reservation.find();
+        res.status(200).json(reservation);
+    }
+    catch (err) {
+        res.status(404).json({error: err});
+    }
+}
 
+exports.removeReservation = async (req, res) => {
+    try {
+        const reservation = await Reservation.deleteOne({_id: req.body.id});
+        res.status(200).json(post);
     }
     catch (err) {
         res.status(404).json({error: err});
---
+++ backend/controllers/userController.js
@@ -0,0 +1,56 @@
+let User = require("../models/User");
+
+exports.checkUsername = async (req, res) => {
+    try {
+        const user = await User.findOne({username: req.body.username});
+        res.status(200).json(user);
+    }
+    catch (err) {
+        res.status(404).json({error: err});
+    }
+}
+
+exports.loadUserByID = async (req, res) => {
+    try {
+        const user = await User.findOne({sessionID: req.body.sessionID});
+        res.status(200).json(user);
+    }
+    catch (err) {
+        res.status(404).json({error: err});
+    }
+}
+
+exports.checkUser = async (req, res) => {
+    try {
+        const user = await User.find({username: req.body.username, password: req.body.password});
+        res.status(200).json(user);
+    }
+    catch (err) {
+        res.status(404).json({error: err});
+    }
+}
+
+exports.saveUser = async (req, res) => {
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password,
+        sessionID: req.body.sessionID
+    });
+
+    try {
+        const savedUser = await user.save();
+        res.status(200).json(savedUser);
+    }
+    catch (err) {
+        res.status(404).json({error: err});
+    }
+};
+
+exports.updateID = (req, res) => {
+    User.findOneAndUpdate({username: req.body.username}, {sessionID: req.body.sessionID}, err => {
+        if (err) {
+            return res.send(500, {error : err});
+        }
+        return res.status(200).json({message: "Onnistui!"});
+    });
+};
