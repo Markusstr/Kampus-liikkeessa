69280169e86549aabadd19fbc56fe35a80f6a3d6
Markusstr <markus.strandman@gmail.com>
2020-01-13 09:41:38 +0200

    Added more routes

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -2,13 +2,37 @@ let express = require('express'),
     router = express.Router();
 
     let reservationController = require('../controllers/reservationController');
+    let userController = require('../controllers/userController');
 
     router.post('/getReservations', (req, res) => {
         reservationController.getReservations(req, res);
     });
 
+    router.post('/removeReservation', (req, res) => {
+        reservationController.removeReservation(req, res);
+    });
+    
     router.post('/saveReservation', (req, res) => {
         reservationController.saveReservation(req, res);
     });
-    
 
+
+    router.post('/checkUsername', (req, res) => {
+        userController.checkUsername(req, res);
+    });
+
+    router.post('/checkUser', (req, res) => {
+        userController.checkUser(req, res);
+    });
+
+    router.post('/loadUserByID', (req, res) => {
+        userController.loadUserByID(req, res);
+    });
+
+    router.post('/updateID', (req, res) => {
+        userController.updateID(req, res);
+    });
+
+    router.post('/saveUser', (req, res) => {
+        userController.saveUser(req, res);
+    });
