1c3c9624fc1206c49e42c1f9854b6a00f0734e92
Markusstr <markus.strandman@gmail.com>
2020-01-13 21:39:40 +0200

    Calendar in main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,56 +1,60 @@
-import React from 'react';
+
+import React, {useState} from 'react';
 import {BrowserRouter as Router,
   Switch,
   Route,
   Link }
 from "react-router-dom";
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 import './App.css';
+import AppBar from './components/general/AppBar';
+import Calendar from './components/routes/calendar';
+import Login from './components/routes/login';
 
 function App() {
+
+  const [loggedStatus, setLoggedStatus] = useState(false);
+
+  function handleLogin() {
+    console.log("Successful function call.");
+    setLoggedStatus(!loggedStatus);
+  }
+
   return (
   <Router>
     {/* Navbar is a specific class and a component, rendered before the router */}
     <div>
-
-      <nav>
-        <ul>
-          <Link to="/home">
-            Home
-          </Link>
-        </ul>
-        <ul>
-          <Link to="/about">
-            About
-          </Link>
-        </ul>
-        <ul>
-          <Link to="/activity">
-            Activity
-          </Link>
-        </ul>
-      </nav>
-
-      <Switch>
-        <Route path="/home">
-          <Home />
-        </Route>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/activity">
-          <Activity />
-        </Route>
-      </Switch>
+        <Switch>
+          <Route exact path="/">
+            <AppBar prop1={loggedStatus}/>
+            <DefaultPort />
+          </Route>
+          <Route path="/login">
+            <Login prop1={handleLogin} prop2={loggedStatus}/>
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/activity">
+            <Activity />
+          </Route>
+        </Switch>
     </div>
   </Router>
   );
 }
 
 // Routers rendered as functions. Replace these with function or class components when components ready 
-function Home () {
-  return (
-    <div>Homepage</div>
-  )
+const testList = [
+  { title: 'Test1' },
+  { title: 'Lappeenranta' },
+  { title: 'Testi' },
+  { title: 'Testaus' },
+];
+
+function DefaultPort() {
+  return (<Calendar/>);
 }
 function About () {
   return (
