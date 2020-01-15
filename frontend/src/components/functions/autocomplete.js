e363ca87ee6d6d7d7c1301fee8d8c483f0a309a8
Markusstr <markus.strandman@gmail.com>
2020-01-15 17:09:58 +0200

    Backend support

---
+++ frontend/src/components/functions/autocomplete.js
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from 'react';
+import TextField from '@material-ui/core/TextField';
+import { Autocomplete } from '@material-ui/lab';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        textAlign: 'center',
+        padding: '5px',
+    },
+    center: {
+        display: 'inline-block',
+    },
+    buttonMargin: {
+        top: "5px",
+    }
+}));
+
+const AutocompleteComponent = props => {
+
+    const classes = useStyles();
+
+    const [data, setData] = useState([])
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+
+        if (!loading) {
+            return undefined;
+        }
+
+        async function fetchData() {
+            try {
+                let response = await fetch("http://localhost:8080/api/getLocations");
+                let jsonData = await response.json()
+                setData(jsonData);
+                setLoading(false);
+            }
+            catch (err) {
+                console.log("Error while loading data");
+            }
+            
+        }
+        fetchData();
+    },[loading]);
+
+    const handleSelect = (event) => {
+        props.prop(event.target.value)
+    }
+
+    return (
+        <div className={classes.root}>
+            <Autocomplete
+                className={classes.center}
+                id="combo-box"
+                options={data}
+                getOptionLabel={option => option.name}
+                loading={loading}
+                onSelect={handleSelect}
+                loadingText="Ladataan..."
+                style={{ width: '50%' }}
+                renderInput={params => (
+                    <TextField {...params} label="Haku" variant="outlined" fullWidth />
+                )}
+            />
+            <br />
+            <Button 
+                className={classes.buttonMargin}
+                style={{ width: 300 }}
+                variant="contained" 
+                color="primary">
+                Uusi varaus
+            </Button>
+        </div>
+    );
+}
+
+export default AutocompleteComponent;
--- frontend/src/components/routes/calendar.js
+++ frontend/src/components/routes/calendar.js
@@ -1,109 +1,198 @@
 import { Calendar, momentLocalizer } from 'react-big-calendar'
-import React from 'react';
+import React, { useState } from 'react';
 import moment from 'moment';
-
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
+import CloseIcon from '@material-ui/icons/Close';
+import "./Calendar.css";
+import { IconButton, Divider } from '@material-ui/core';
+
+import AutocompleteComponent from '../functions/autocomplete';
 import { useEffect } from 'react';
 
 const BigCalendar = () => {
-  let localizer;
+    let localizer, events = [];
 
-  useEffect(() => {
-  });
+    const [location, setLocation] = useState('');
+    const [open, setOpen] = useState(false);
+    const [eventInfo, setEventInfo] = useState({
+        name: '',
+        info: '',
+        start: new Date(),
+        end: new Date(),
+        location: ''
+    });
 
-  moment.locale('fi', {
-    months: 'tammikuu_helmikuu_maaliskuu_huhtikuu_toukokuu_kesäkuu_heinäkuu_elokuu_syyskuu_lokakuu_marraskuu_joulukuu'.split('_'),
-    monthsShort: 'tammi_helmi_maalis_huhti_touko_kesä_heinä_elo_syys_loka_marras_joulu'.split('_'),
-    monthsParseExact: true,
-    weekdays: 'sunnuntai_maanantai_tiistai_keskiviikko_torstai_perjantai_lauantai'.split('_'),
-    weekdaysShort: 'su_ma_ti_ke_to_pe_la'.split('_'),
-    weekdaysMin: 'Su_Ma_Ti_Ke_To_Pe_La'.split('_'),
-    weekdaysParseExact: true,
+    useEffect(() => {
+        async function fetchData() {
+            const bodyData = {
+                location: location
+            };
+            try {
+                let response = await fetch("http://localhost:8080/api/getReservations", {
+                    method: "post",
+                    headers: {"Content-Type":"application/json"},
+                    body: JSON.stringify(bodyData)
+                });
+                events = await response.json();
+                console.log(events);
+            }
+            catch (err) {
+                console.log(err);
+            }
+        }
 
-    longDateFormat : {
-      LT : 'HH:mm',
-      LTS : 'HH:mm:ss',
-      L : 'DD/MM/YYYY',
-      LL : 'D MMMM YYYY',
-      LLL : 'D MMMM YYYY HH:mm',
-      LLLL : 'dddd D MMMM YYYY HH:mm'
-    },
-    calendar : {
-      sameDay : '[Tänään klo] LT',
-      nextDay : '[Huomenna klo] LT',
-      nextWeek : 'dddd [klo] LT',
-      lastDay : '[Eilen klo] LT',
-      lastWeek : 'dddd [Viime viikolla klo] LT',
-      sameElse : 'L'
-    },
-    relativeTime : {
-      future : '%s/s kuluttua',
-      past : '%s/s sitten',
-      s : 'muutama sekunti',
-      m : 'minuutti',
-      mm : '%d minuuttia',
-      h : 'tunti',
-      hh : '%d tuntia',
-      d : 'päivä',
-      dd : '%d päivää',
-      M : 'kuukausi',
-      MM : '%d kuukautta',
-      y : 'vuosi',
-      yy : '%d vuotta'
-    },
-    week: {
-      dow : 1,
+        fetchData();
+
+    },[location]);
+
+    const setItemLocation = params1 => {
+        setLocation(params1);
     }
-  });
+
+    const handleClickOpen = event => {
+        setOpen(true);
+        setEventInfo(event);
+    };
+    
+    const handleClose = () => {
+        setOpen(false);
+    };
+    
+    moment.updateLocale('fi', {
+        months: 'tammikuu_helmikuu_maaliskuu_huhtikuu_toukokuu_kesäkuu_heinäkuu_elokuu_syyskuu_lokakuu_marraskuu_joulukuu'.split('_'),
+        monthsShort: 'tammi_helmi_maalis_huhti_touko_kesä_heinä_elo_syys_loka_marras_joulu'.split('_'),
+        monthsParseExact: true,
+        weekdays: 'sunnuntai_maanantai_tiistai_keskiviikko_torstai_perjantai_lauantai'.split('_'),
+        weekdaysShort: 'su_ma_ti_ke_to_pe_la'.split('_'),
+        weekdaysMin: 'Su_Ma_Ti_Ke_To_Pe_La'.split('_'),
+        weekdaysParseExact: true,
+
+        longDateFormat : {
+        LT : 'HH:mm',
+        LTS : 'HH:mm:ss',
+        L : 'DD/MM/YYYY',
+        LL : 'D MMMM YYYY',
+        LLL : 'D MMMM YYYY HH:mm',
+        LLLL : 'dddd D MMMM YYYY HH:mm'
+        },
+        calendar : {
+        sameDay : '[Tänään klo] LT',
+        nextDay : '[Huomenna klo] LT',
+        nextWeek : 'dddd [klo] LT',
+        lastDay : '[Eilen klo] LT',
+        lastWeek : 'dddd [Viime viikolla klo] LT',
+        sameElse : 'L'
+        },
+        relativeTime : {
+        future : '%s/s kuluttua',
+        past : '%s/s sitten',
+        s : 'muutama sekunti',
+        m : 'minuutti',
+        mm : '%d minuuttia',
+        h : 'tunti',
+        hh : '%d tuntia',
+        d : 'päivä',
+        dd : '%d päivää',
+        M : 'kuukausi',
+        MM : '%d kuukautta',
+        y : 'vuosi',
+        yy : '%d vuotta'
+        },
+        week: {
+        dow : 1,
+        }
+    });
 
 
-  /* const ColoredDateCellWrapper = ({ children }) =>
-    React.cloneElement(React.Children.only(children), {
-      style: {
-        backgroundColor: 'lightblue',
-      },
-    }) */
-  const events = [
-    {
-        title: 'All Day Event very long title',
-        start: new Date(2020, 0, 15, 12, 30),
-        end: new Date(2020, 0, 15, 14, 30),
-    }
-  ];
+    /* const ColoredDateCellWrapper = ({ children }) =>
+        React.cloneElement(React.Children.only(children), {
+        style: {
+            backgroundColor: 'lightblue',
+        },
+        }) */
+    /*const events = [
+        {
+            name: 'Markus',
+            info: 'Koripallovuoro',
+            start: new Date(2020, 0, 16, 12, 30),
+            end: new Date(2020, 0, 16, 14, 30),
+            location: "Monitoimisali"
+        }
+    ];*/
 
-  const messages = {
-    allDay: 'Koko päivä',
-    previous: '<',
-    next: '>',
-    today: 'Tänään',
-    month: 'Kuukausi',
-    week: 'Viikko',
-    day: 'Päivä',
-    agenda: 'Agenda',
-    date: 'Päivämäärä',
-    time: 'Aika',
-    event: 'Tapahtuma',
-    showMore: total => `+ Näytä lisää (${total})`
-  };
+    const messages = {
+        allDay: 'Koko päivä',
+        previous: '<',
+        next: '>',
+        today: 'Tänään',
+        month: 'Kuukausi',
+        week: 'Viikko',
+        day: 'Päivä',
+        agenda: 'Agenda',
+        date: 'Päivämäärä',
+        time: 'Aika',
+        event: 'Tapahtuma',
+        showMore: total => `+ Näytä lisää (${total})`
+    };
 
-  let date = new Date()
-  
-  localizer = momentLocalizer(moment);
+    let date = new Date()
+    
+    localizer = momentLocalizer(moment);
 
-  return(
-    <div>
-        <Calendar messages={messages}
-          localizer={localizer}
-          events={events}
-          views={["week", "month"]}
-          defaultView={"week"}
-          scrollToTime={new Date(date.getFullYear(), date.getMonth(), date.getDate(), 8)}
-          startAccessor="start"
-          endAccessor="end"
-          style={{ height: 700 }}
-        />
-    </div>
-  );
+    return(
+        <div>
+            <AutocompleteComponent prop={setItemLocation}/>
+
+            <p>Valittu tila: {location}</p>
+
+            <Calendar className="calendar-container"
+                messages={messages}
+                localizer={localizer}
+                events={events}
+                views={["week", "month"]}
+                defaultView={"week"}
+                defaultDate={new Date()}
+                onSelectEvent={handleClickOpen}
+                titleAccessor={'info'}
+                scrollToTime={new Date(date.getFullYear(), date.getMonth(), date.getDate(), 8)}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: 750 }}
+                />
+                <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+                    <DialogTitle disableTypography className="top-buttons">
+                        {eventInfo.location}
+                        <IconButton>
+                            <CloseIcon />
+                        </IconButton>
+                    </DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            Aika: {eventInfo.start.getDate()}.{eventInfo.start.getMonth()+1}.{eventInfo.start.getFullYear()} klo {eventInfo.start.getHours()}:{eventInfo.start.getMinutes()} - {eventInfo.end.getHours()}:{eventInfo.end.getMinutes()}<br />
+                            Varaaja: {eventInfo.name}<br />
+                            Lisätietoja: {eventInfo.info}
+                            
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions className="bottom-buttons">
+                        <IconButton>
+                            <EditIcon />
+                        </IconButton>
+                        <IconButton>
+                            <DeleteIcon />
+                        </IconButton>
+                    </DialogActions>
+                </Dialog>
+        </div>
+    );
 }
 
 export default BigCalendar;
